795d7de59d2298daa1514f2cbccca45e8fdeb6e7
Rosita <natsukiarr@gmail.com>
2025-04-05 12:20:58 -0700

    ADD: Bootstrap css and 404 page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,6 @@
 import { Routes } from '@angular/router';
+import { NotFound404Component } from './pages/not-found404/not-found404.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: "**", component: NotFound404Component }
+];
