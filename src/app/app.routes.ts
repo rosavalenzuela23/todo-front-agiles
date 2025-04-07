ab0d0807e3290e10cc688a5838088c73e3433354
Rosita <natsukiarr@gmail.com>
2025-04-07 16:23:38 -0700

    change: pagina principal

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,9 +2,10 @@ import { Routes } from '@angular/router';
 import { NotFound404Component } from './pages/not-found404/not-found404.component';
 import { MistareasComponent } from './pages/mistareas/mistareas.component';
 import { AgregarTareaComponent } from './pages/agregar-tarea/agregar-tarea.component';
+import { DashboardComponent } from './pages/dashboard/dashboard.component';
 
 export const routes: Routes = [
-    { path: "mistareas", component: MistareasComponent },
+    { path: "dashboard", component: DashboardComponent },
     { path: "agregar", component: AgregarTareaComponent },
     { path: "editar", component: AgregarTareaComponent },
     { path: "**", component: NotFound404Component }
