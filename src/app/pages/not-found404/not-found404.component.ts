391349590321342196bab0e45f1c33a83fe78ba3
Rosita <natsukiarr@gmail.com>
2025-04-07 17:07:37 -0700

    ADD: Funcionalidad al boton de regresar 404

--- src/app/pages/not-found404/not-found404.component.ts
+++ src/app/pages/not-found404/not-found404.component.ts
@@ -1,11 +1,19 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-not-found404',
   imports: [],
   templateUrl: './not-found404.component.html',
-  styleUrl: './not-found404.component.css'
+  styleUrl: './not-found404.component.css',
 })
 export class NotFound404Component {
-
+  
+  constructor(
+    private router: Router
+  ){}
+  
+  regresarAlDashboard() {
+    this.router.navigate(['dashboard'])
+  }
 }
