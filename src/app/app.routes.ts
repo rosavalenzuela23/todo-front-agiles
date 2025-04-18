8f48005142a7555993f44eac077ec56feef2bde6
Marcos-Ruiz1 <Marcos.ZazuetaRuiz1@gmail.com>
2025-04-18 13:02:10 -0700

    ADD: Vista de iniciar sesión
    Se agrego la vista y lógica de la funcionalidad de iniciar sesión. Además se ha agregado refactorización de el nombre de carpetas y routing para el log in.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,7 +3,8 @@ import { NotFound404Component } from './pages/not-found404/not-found404.componen
 import { MistareasComponent } from './pages/mistareas/mistareas.component';
 import { AgregarTareaComponent } from './pages/agregar-tarea/agregar-tarea.component';
 import { DashboardComponent } from './pages/dashboard/dashboard.component';
-import {RegistrarusuarioComponent} from './pages/registrarusuario/registrarusuario.component';
+import {RegistrarusuarioComponent} from './pages/registrar-usuario/registrarusuario.component';
+import { IniciarSesionComponent } from './pages/iniciar-sesion/iniciarsesion.component';
 
 export const routes: Routes = [
     { path: "dashboard", component: DashboardComponent },
@@ -11,5 +12,6 @@ export const routes: Routes = [
     { path: "editar", component: AgregarTareaComponent },
     { path: "mistareas", component: MistareasComponent },
     {path: "registrar", component: RegistrarusuarioComponent},
+    {path: "login", component: IniciarSesionComponent},
     { path: "**", component: NotFound404Component }
 ];
